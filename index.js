6c342909255e8469fd1ddaa9187d421ea3926b41
Aditya Kulkarni <kulkarniaditya1997@gmail.com>
2023-07-13 12:46:09 +0530

    Changes for starter code

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import MWAComponent from './components/MWAComponent';
 
 window.addEventListener = () => {};
 window.removeEventListener = () => {};
 
 AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(
-  'MobileWalletAdapterEntrypoint',
-  () => MWAComponent,
-);
